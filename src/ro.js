a594ba61a3f5d7af3f44954ae26e925e2aff13a1
Matheus Cintra <42045423+matheus-cintra@users.noreply.github.com>
2019-05-07 17:21:48 -0300

    Added Roraima s aValidator

---
+++ src/ro.js
@@ -0,0 +1,57 @@
+/************************************************
+ * RO - IE validator for Roraima state
+ ************************************************/
+
+let h = require('../util/helper');
+
+/**
+ * @name validate
+ * @description
+ * Check if the ie (inscrição estadual) representing by state is a valid number
+ * technical specification: http://www.sintegra.gov.br/Cad_Estados/cad_RO.html
+ * example: '5833583880755-9'
+ *
+ * @param {string} ie string representing the brazilian state registration for companies
+ *
+ * @returns {boolean}
+ */
+
+function validate(ie) {
+    if (!ie) return false;
+    if (typeof ie !== 'string') ie = ie.toString();
+    ie = h.returnOnlyNumbers(ie);
+    if (ie.length !== 14) return false;
+    return weightCalculator(ie);
+}
+
+/**
+ * @name weightCalculator
+ * @description
+ * Calculate the weight according the technical specification
+ *
+ * @param {string|Array} ie number registration
+ * @param {string|number} from base (first weightCalculation)
+ */
+
+function weightCalculator(ie) {
+    let weights = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+    let base = 0;
+    let block = ie.substring(0, ie.length - 1).split('');
+
+    if (block.length !== weights.length) return false;
+    for (let i = 0; i < block.length; i++) base += weights[i] * block[i];
+
+    resto = base % 11;
+    dig = 11 - resto;
+
+    if (dig == 10 || dig == 11) {
+        dig -= 10;
+    }
+
+    block.push((dig).toString());
+    let i = block.join().replace(/,/g, '');
+
+    return h.mask(i, '#############-#');
+}
+
+module.exports = validate;
