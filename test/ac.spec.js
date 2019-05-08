436868a2590e04a3821831a8f4a8b7ab88e3c54a
Matheus Cintra <42045423+matheus-cintra@users.noreply.github.com>
2019-05-08 17:48:15 -0300

    Fixed Acre state for IE validator.

---
+++ test/ac.spec.js
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * AC - IE validator specification test file for Acre state
+ * *****************************************************************************
+ * technical specification: http://www.sintegra.gov.br/Cad_Estados/cad_AC.html
+ * example: '01.004.823/001-12'
+ ******************************************************************************/
+
+let assert = require('assert');
+let ac = require('../src/ac');
+
+/**
+ * the ie variable representing a valid I.E. for this state function and will be
+ * pass if the function './src/ac.js' is correct as technical specification.
+ * It will fail if the functions is wrong or the ie number is incompatible with state
+ */
+let ie = '01.500.496/141-02';
+
+describe(`Checking the I.E. number '${ie}'...`, () => {
+    it('ac', () => {
+        assert.equal(ac(ie), ie,
+            `Sorry... the I.E. number '${ie}' for the 'AC' state is not valid! :(`);
+    });
+});
