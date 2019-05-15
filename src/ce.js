72ea0b8bcd24e45250ee00f4669c59c248b1e142
jsmiranda12 <50247175+jsmiranda12@users.noreply.github.com>
2019-05-15 11:32:57 -0300

    Update ce.js
    Fixed some commentaries

--- src/ce.js
+++ src/ce.js
@@ -1,5 +1,5 @@
 /************************************************
- * AC - IE validator for Acre state
+ * AC - IE validator for Ceará state
  ************************************************/
 
 let h = require("../util/helper");
@@ -8,8 +8,8 @@ let h = require("../util/helper");
  * @name validate
  * @description
  * Check if the ie (inscrição estadual) representing by state is a valid number
- * technical specification: http://www.sintegra.gov.br/Cad_Estados/cad_AC.html
- * example: '01.004.823/001-12'
+ * technical specification: http://www.sintegra.gov.br/Cad_Estados/cad_CE.html
+ * example: '57735951-7'
  *
  * @param {string} ie string representing the brazilian state registration for companies
  *
