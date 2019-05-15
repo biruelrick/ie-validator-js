62a9978a7a3d8d7858ea76dd02698d947c401efa
jsmiranda12 <50247175+jsmiranda12@users.noreply.github.com>
2019-05-15 11:23:13 -0300

    Update ap.js
    Fixes and added some comentaries

--- src/ap.js
+++ src/ap.js
@@ -1,16 +1,18 @@
 /************************************************
- * AP - IE validator for state
+ * AP - IE validator for Amapá state
  ************************************************/
 let h = require("../util/helper");
+
 /**
  * @name validate
  * @description
  * Check if the ie (inscrição estadual) representing by state is a valid number
- *
+ * technical specification: http://www.sintegra.gov.br/Cad_Estados/cad_AP.html
  * @param {string} ie string representing the brazilian state registration for companies
  *
  * @returns {boolean}
  */
+
 function validate(ie) {
   if (!ie) return false;
   if (typeof ie !== "string") ie = ie.toString();
@@ -23,13 +25,13 @@ function validate(ie) {
 
   return weightCalculator(ie);
 
-  /**
-   * @name weightCalculator
-   * @description
-   * Calculate the weight according the technical specification
-   *
-   * @param {string|Array} ie number registration
-   */
+/**
+ * @name weightCalculator
+ * @description
+ * Calculate the weight according the technical specification
+ *
+ * @param {string|Array} ie number registration
+ */
   function weightCalculator(ie) {
     let weights = [9, 8, 7, 6, 5, 4, 3, 2];
     let base = 0;
