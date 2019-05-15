b14b61b5aca3d310989f424cbfb9f97630d95226
jsmiranda12 <50247175+jsmiranda12@users.noreply.github.com>
2019-05-15 12:06:59 -0300

    Update sp.js

--- src/sp.js
+++ src/sp.js
@@ -1,5 +1,5 @@
 /************************************************
- * AC - IE validator for Acre state
+ * AC - IE validator for São Paulo state
  ************************************************/
 
 let h = require("../util/helper");
@@ -8,8 +8,8 @@ let h = require("../util/helper");
  * @name validate
  * @description
  * Check if the ie (inscrição estadual) representing by state is a valid number
- * technical specification: http://www.sintegra.gov.br/Cad_Estados/cad_AC.html
- * example: '01.004.823/001-12'
+ * technical specification: http://www.sintegra.gov.br/Cad_Estados/cad_SP.html
+ * example: '907.141.753.703'
  *
  * @param {string} ie string representing the brazilian state registration for companies
  * @param {string|Boolean} pd if user is a 'Produtor Rural'
